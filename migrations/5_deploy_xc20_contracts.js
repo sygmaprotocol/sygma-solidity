14d45a193b0e51679cfd3234ddd7e58a55ae7189
nmlinaric <nikola.mlinaric93@gmail.com>
2022-11-15 08:31:22 +0100

    Update XC20 migration

--- migrations/5_deploy_xc20_contracts.js
+++ migrations/5_deploy_xc20_contracts.js
@@ -10,6 +10,7 @@ const networksConfig = JSON.parse(fs.readFileSync("./networks_config.json"));
 const BridgeContract = artifacts.require("Bridge");
 const XC20SafeContract = artifacts.require("XC20Safe");
 const XC20HandlerContract = artifacts.require("XC20Handler");
+const XC20TestContract = artifacts.require("XC20Test");
 const FeeRouterContract = artifacts.require("FeeHandlerRouter");
 const FeeHandlerWithOracleContract = artifacts.require("FeeHandlerWithOracle");
 const ERC20MinterPauserContract = artifacts.require("ERC20PresetMinterPauser");
@@ -26,15 +27,21 @@ module.exports = async function (deployer, network) {
     const feeHandlerWithOracleInstance = await FeeHandlerWithOracleContract.deployed();
     const erc20MinterPauserInstance = await ERC20MinterPauserContract.deployed();
 
-    // deploy XC20Safe contract
+    // deploy XC20 contracts
     await deployer.deploy(XC20SafeContract);
+    const xc20TestInstance = await deployer.deploy(XC20TestContract, erc20MinterPauserInstance.address);
     await deployer.deploy(XC20HandlerContract, bridgeInstance.address);
     const xc20HandlerInstance = await XC20HandlerContract.deployed();
 
-    console.log("XC20SafeHandler contract address:", "\t", xc20HandlerInstance.address);
+    console.log("XC20Handler contract address:", "\t", xc20HandlerInstance.address);
+    console.log("XC20Test contract address:", "\t", xc20TestInstance.address);
 
     // setup XC20Handler
+    await bridgeInstance.adminSetResource(xc20HandlerInstance.address, currentNetworkConfig.xc20ResourceID, xc20TestInstance.address);
     await bridgeInstance.adminChangeFeeHandler(feeRouterInstance.address);
+    await erc20MinterPauserInstance.grantRole(await erc20MinterPauserInstance.MINTER_ROLE(), xc20HandlerInstance.address);
+    await bridgeInstance.adminSetBurnable(xc20HandlerInstance.address, xc20TestInstance.address);
+
 
     // set resourceID for every network except current from networks config
     delete networksConfig[currentNetworkName]
