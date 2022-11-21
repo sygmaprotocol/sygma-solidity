888f205f388172bdc0b193707a6a87c848bbd282
Matija Petrunic <matija.petrunic@gmail.com>
2022-11-21 16:49:28 +0100

    Refactor 2nd migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -7,15 +7,15 @@ const Ethers = require("ethers");
 const fs = require("fs");
 
 const Helpers = require('../test/helpers');
+const Utils = require('./utils');
 
-const networksConfig = JSON.parse(fs.readFileSync("./networks_config.json"));
+const networksConfig = JSON.parse(fs.readFileSync("./devnet_config.json"));
 
 const AccessControlSegregatorContract = artifacts.require("AccessControlSegregator");
 const PausableContract = artifacts.require("Pausable");
 const BridgeContract = artifacts.require("Bridge");
 const TestStoreContract = artifacts.require("TestStore");
-const ERC20MinterPauserContract = artifacts.require("ERC20PresetMinterPauser");
-const ERC20LockReleaseContract = artifacts.require("ERC20PresetMinterPauser");
+const ERC20PresetMinterPauser = artifacts.require("ERC20PresetMinterPauser");
 const ERC20SafeContract = artifacts.require("ERC20Safe");
 const ERC721MinterBurnerPauserContract = artifacts.require("ERC721MinterBurnerPauser");
 const ERC721SafeContract = artifacts.require("ERC721Safe");
@@ -26,7 +26,6 @@ const FeeRouterContract = artifacts.require("FeeHandlerRouter");
 const BasicFeeHandlerContract = artifacts.require("BasicFeeHandler");
 const FeeHandlerWithOracleContract = artifacts.require("FeeHandlerWithOracle");
 
-
 module.exports = async function(deployer, network) {
     // fetch deployer address
     const deployerAddress = await deployer['networks'][deployer['network']]['from'];
@@ -36,18 +35,14 @@ module.exports = async function(deployer, network) {
     // trim suffix from network name and fetch current network config
     let currentNetworkName = network.split("-")[0]
     let currentNetworkConfig = networksConfig[network.split("-")[0]]
+    delete networksConfig[currentNetworkName]
+
     // deploy utils contracts
     const accessControlSegregatorInstance = await deployer.deploy(AccessControlSegregatorContract, Helpers.accessControlFuncSignatures, functionAccessAddresses);
     await deployer.deploy(PausableContract);
 
     // deploy main contracts
     const bridgeInstance = await deployer.deploy(BridgeContract, currentNetworkConfig.domainID, accessControlSegregatorInstance.address);
-    const TestStoreInstance = await deployer.deploy(TestStoreContract);
-    const erc20MinterPauserInstance = await deployer.deploy(ERC20MinterPauserContract, currentNetworkConfig.erc20Name, currentNetworkConfig.erc20Symbol);
-    await deployer.deploy(ERC20SafeContract);
-    const erc20LockReleaseInstance = await deployer.deploy(ERC20LockReleaseContract, currentNetworkConfig.erc20LRName, currentNetworkConfig.erc20LRSymbol);
-    const erc721MinterBurnerPauserInstance = await deployer.deploy(ERC721MinterBurnerPauserContract, currentNetworkConfig.erc721Name, currentNetworkConfig.erc721Symbol, currentNetworkConfig.erc721URI);
-    await deployer.deploy(ERC721SafeContract);
 
     // deploy handler contracts
     const erc20HandlerInstance = await deployer.deploy(ERC20HandlerContract, bridgeInstance.address);
@@ -59,72 +54,98 @@ module.exports = async function(deployer, network) {
     const basicFeeHandlerInstance = await deployer.deploy(BasicFeeHandlerContract, bridgeInstance.address, feeRouterInstance.address);
     const feeHandlerWithOracleInstance = await deployer.deploy(FeeHandlerWithOracleContract, bridgeInstance.address, feeRouterInstance.address);
 
-    // fetch deployed network domainID
-    const domainID = (await (await BridgeContract.deployed())._domainID()).toString();
-
-    console.log("🎉🎉🎉 Sygma contracts successfully deployed 🎉🎉🎉","\n");
-    console.log("===================================================");
-    console.table({
-        "Deployer Address": deployerAddress,
-        "Domain ID": domainID,
-        "Bridge Address": bridgeInstance.address,
-        "TestStore Address": TestStoreInstance.address,
-        "ERC20 Address": erc20MinterPauserInstance.address,
-        "ERC20LockRelease Address": erc20LockReleaseInstance.address,
-        "ERC721 Address": erc721MinterBurnerPauserInstance.address,
-        "ERC20Handler Address": erc20HandlerInstance.address,
-        "ERC20LockReleaseHandler Address": erc20HandlerInstance.address,
-        "ERC721Handler Address": erc721HandlerInstance.address,
-        "GenericHandler Address": genericHandlerInstance.address,
-        "FeeRouterContract Address": feeRouterInstance.address,
-        "BasicFeeHandler Address": basicFeeHandlerInstance.address,
-        "FeeHandlerWithOracle Address": feeHandlerWithOracleInstance.address,
-        "ERC20 resourceID": currentNetworkConfig.erc20ResourceID,
-        "Generic resourceID": currentNetworkConfig.genericResourceID,
-        "ERC721 resourceID": currentNetworkConfig.erc721ResourceID,
-        "ERCLockRelease20 resourceID": currentNetworkConfig.erc20LockReleaseResourceID
-    });
-
-    /* setup contracts */
-    const tokenAmount = process.env.TOKEN_AMOUNT;
-    const feeOracleAddress = process.env.FEE_ORACLE_ADDRESS;
-    const feeHandlerWithOracleGasUsed = process.env.FHWO_GAS_USED;
-    const feeHandlerWithOracleFeePercentage = process.env.FHWO_FEE_PERCENTAGE; // e.g. 500 means 5%
-    const basicFeeHandlerFee = Ethers.utils.parseEther(process.env.BFH_FEE);
-
-    // setup ERC20
-    await bridgeInstance.adminSetResource(erc20HandlerInstance.address, currentNetworkConfig.erc20ResourceID, erc20MinterPauserInstance.address);
-    await bridgeInstance.adminSetResource(erc20HandlerInstance.address, currentNetworkConfig.erc20LockReleaseResourceID, erc20LockReleaseInstance.address);
-    await erc20MinterPauserInstance.grantRole(await erc20MinterPauserInstance.MINTER_ROLE(), erc20HandlerInstance.address);
-    await erc20MinterPauserInstance.mint(deployerAddress, tokenAmount);
-    await erc20MinterPauserInstance.mint(erc20HandlerInstance.address, tokenAmount);
-    await erc20MinterPauserInstance.approve(erc20HandlerInstance.address, tokenAmount);
-    await bridgeInstance.adminSetBurnable(erc20HandlerInstance.address, erc20MinterPauserInstance.address);
-
-    // setup ERC721
-    await bridgeInstance.adminSetResource(erc721HandlerInstance.address, currentNetworkConfig.erc721ResourceID, erc721MinterBurnerPauserInstance.address);
-    await erc721MinterBurnerPauserInstance.grantRole(await erc20MinterPauserInstance.MINTER_ROLE(), erc721HandlerInstance.address);
-    await bridgeInstance.adminSetBurnable(erc721HandlerInstance.address, erc721MinterBurnerPauserInstance.address);
-
-    // setup generic
-    await bridgeInstance.adminSetGenericResource(genericHandlerInstance.address, currentNetworkConfig.genericResourceID, TestStoreInstance.address, Helpers.blankFunctionSig, Helpers.blankFunctionDepositorOffset, Helpers.getFunctionSignature(TestStoreInstance, 'store'));
+    // deploy safe contracts
+    await deployer.deploy(ERC20SafeContract);
+    await deployer.deploy(ERC721SafeContract);
 
     // setup fee router and fee handlers
     await bridgeInstance.adminChangeFeeHandler(feeRouterInstance.address);
-    await feeHandlerWithOracleInstance.setFeeOracle(feeOracleAddress);
-    await feeHandlerWithOracleInstance.setFeeProperties(feeHandlerWithOracleGasUsed, feeHandlerWithOracleFeePercentage);
-    await basicFeeHandlerInstance.changeFee(basicFeeHandlerFee.toString());
+    await feeHandlerWithOracleInstance.setFeeOracle(currentNetworkConfig.fee.oracle.address);
+    await feeHandlerWithOracleInstance.setFeeProperties(currentNetworkConfig.fee.oracle.gasUsed, currentNetworkConfig.fee.oracle.feePercentage);
+    await basicFeeHandlerInstance.changeFee(Ethers.utils.parseEther(currentNetworkConfig.fee.basic.fee).toString());
+
+    // setup erc20 tokens
+    for (const erc20 of currentNetworkConfig.erc20) {
+      await setupErc20(deployer, erc20, bridgeInstance, erc20HandlerInstance);
+      await Utils.setupFee(networksConfig, feeRouterInstance, feeHandlerWithOracleInstance, basicFeeHandlerInstance, erc20);
+    }
+
+    // setup erc721 tokens
+    for (const erc721 of currentNetworkConfig.erc721) {
+      await setupErc721(deployer, erc721, bridgeInstance, erc721HandlerInstance);
+      await Utils.setupFee(networksConfig, feeRouterInstance, feeHandlerWithOracleInstance, basicFeeHandlerInstance, erc721);
+    }
+
+    for (const generic of currentNetworkConfig.permissionedGeneric) {
+      await setupGeneric(deployer, generic, bridgeInstance, genericHandlerInstance);
+      await Utils.setupFee(networksConfig, feeRouterInstance, feeHandlerWithOracleInstance, basicFeeHandlerInstance);
+    }
 
     // set MPC address
-    await bridgeInstance.endKeygen(currentNetworkConfig.MPCAddress);
+    if (currentNetworkConfig.MPCAddress) await bridgeInstance.endKeygen(currentNetworkConfig.MPCAddress);
 
-    // set resourceID for every network except current from networks config
-    delete networksConfig[currentNetworkName]
-    for await (const network of Object.values(networksConfig)) {
-      await feeRouterInstance.adminSetResourceHandler(network.domainID, network.erc20ResourceID, feeHandlerWithOracleInstance.address)
-      await feeRouterInstance.adminSetResourceHandler(network.domainID, network.erc20LockReleaseResourceID, basicFeeHandlerInstance.address)
-      await feeRouterInstance.adminSetResourceHandler(network.domainID, network.erc721ResourceID, basicFeeHandlerInstance.address)
-      await feeRouterInstance.adminSetResourceHandler(network.domainID, network.genericResourceID, basicFeeHandlerInstance.address)
-    }
     console.log("🎉🎉🎉 Sygma bridge successfully configured 🎉🎉🎉","\n");
+}
+
+async function setupErc20(
+  deployer,
+  erc20,
+  bridgeInstance,
+  erc20HandlerInstance
+) {
+  var erc20Instance
+  if (!erc20.address) {
+    erc20Instance = await deployer.deploy(ERC20PresetMinterPauser, erc20.name, erc20.symbol);
+    erc20.address = erc20Instance.address
+  } else {
+    erc20Instance = await ERC20PresetMinterPauser.at(erc20.address)
+    erc20Instance.contract.setProvider(deployer.provider)
+  }
+
+  await bridgeInstance.adminSetResource(erc20HandlerInstance.address, erc20.resourceID, erc20Instance.address);
+
+  if (erc20.strategy == "mb") {
+    await erc20Instance.grantRole(await erc20Instance.MINTER_ROLE(), erc20HandlerInstance.address);
+    await bridgeInstance.adminSetBurnable(erc20HandlerInstance.address, erc20Instance.address);
+  }
+}
+
+async function setupErc721(
+  deployer,
+  erc721,
+  bridgeInstance,
+  erc721HandlerInstance
+) {
+  var erc721Instance
+  if (!erc721.address) {
+    erc721Instance = await deployer.deploy(ERC721MinterBurnerPauserContract, erc721.name, erc721.symbol, erc721.uri);
+    erc721.address = erc721Instance.address
+  } else {
+    erc721Instance = await ERC721MinterBurnerPauserContract.at(erc721.address)
+    erc721Instance.contract.setProvider(deployer.provider)
+  }
+
+  await bridgeInstance.adminSetResource(erc721HandlerInstance.address, erc721.resourceID, erc721.address);
+
+  await erc721Instance.grantRole(await erc721Instance.MINTER_ROLE(), erc721HandlerInstance.address);
+  await bridgeInstance.adminSetBurnable(erc721HandlerInstance.address, erc721Instance.address);
+}
+
+async function setupGeneric(deployer, generic, bridgeInstance, genericHandlerInstance) {
+  if (!generic.address) {
+    const testStoreInstance = await deployer.deploy(TestStoreContract);
+    generic.address = testStoreInstance.address;
+    generic.functionSignature = Helpers.blankFunctionSig;
+    generic.depositorOffset = Helpers.blankFunctionDepositorOffset;
+    generic.functionSig = Helpers.getFunctionSignature(testStoreInstance, "store");
+  }
+
+  await bridgeInstance.adminSetGenericResource(
+    genericHandlerInstance.address,
+    generic.resourceID,
+    generic.address,
+    Helpers.blankFunctionSig,
+    Helpers.blankFunctionDepositorOffset,
+    Helpers.getFunctionSignature(TestStoreInstance, 'store')
+  );
 }
