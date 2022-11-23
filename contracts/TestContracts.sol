a3d0ab7c2aa692a041cff6380223a94712b828a4
nmlinaric <nikola.mlinaric93@gmail.com>
2022-11-23 10:25:31 +0100

    Fix indentation

--- contracts/TestContracts.sol
+++ contracts/TestContracts.sol
@@ -152,10 +152,10 @@ contract XC20Test is ERC20 {
     constructor() ERC20("XC20Test", "XC20TST") {}
 
     function mint(address to, uint256 amount) public {
-      _mint(to, amount);
+        _mint(to, amount);
     }
 
     function burn(address from, uint256 amount) public {
-      _burn(from, amount);
+        _burn(from, amount);
     }
 }
