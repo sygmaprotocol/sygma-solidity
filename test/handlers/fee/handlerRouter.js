4463bcb03fd046875e8109fa5e9266ffdc304015
Matija Petrunić <matija.petrunic@gmail.com>
2023-10-02 13:24:42 +0200

    feat: Implement fee whitelist (#203)

--- test/handlers/fee/handlerRouter.js
+++ test/handlers/fee/handlerRouter.js
@@ -1,18 +1,25 @@
 // The Licensed Work is (c) 2022 Sygma
 // SPDX-License-Identifier: LGPL-3.0-only
 
+const Ethers = require("ethers");
+
 const TruffleAssert = require("truffle-assertions");
 
 const Helpers = require("../../helpers");
 
-const DynamicFeeHandlerContract = artifacts.require("DynamicERC20FeeHandlerEVM");
+const BasicFeeHandlerContract = artifacts.require("BasicFeeHandler");
 const FeeHandlerRouterContract = artifacts.require("FeeHandlerRouter");
 const ERC20MintableContract = artifacts.require("ERC20PresetMinterPauser");
 
 contract("FeeHandlerRouter", async (accounts) => {
   const originDomainID = 1;
   const destinationDomainID = 2;
+  const feeData = "0x0";
   const nonAdmin = accounts[1];
+  const whitelistAddress = accounts[2];
+  const nonWhitelistAddress = accounts[3];
+  const recipientAddress = accounts[3];
+  const bridgeAddress = accounts[4];
 
   const assertOnlyAdmin = (method, ...params) => {
     return TruffleAssert.reverts(
@@ -21,27 +28,23 @@ contract("FeeHandlerRouter", async (accounts) => {
     );
   };
 
-  let BridgeInstance;
   let FeeHandlerRouterInstance;
+  let BasicFeeHandlerInstance;
   let ERC20MintableInstance;
   let resourceID;
 
   beforeEach(async () => {
     await Promise.all([
-      (BridgeInstance = await Helpers.deployBridge(
-        destinationDomainID,
-        accounts[0]
-      )),
       ERC20MintableContract.new("token", "TOK").then(
         (instance) => (ERC20MintableInstance = instance)
       ),
     ]);
 
     FeeHandlerRouterInstance = await FeeHandlerRouterContract.new(
-      BridgeInstance.address
+      bridgeAddress
     );
-    DynamicFeeHandlerInstance = await DynamicFeeHandlerContract.new(
-      BridgeInstance.address,
+    BasicFeeHandlerInstance = await BasicFeeHandlerContract.new(
+      bridgeAddress,
       FeeHandlerRouterInstance.address
     );
 
@@ -80,6 +83,146 @@ contract("FeeHandlerRouter", async (accounts) => {
       destinationDomainID,
       resourceID,
       feeHandlerAddress
+    );
+  });
+
+  it("should successfully set whitelist on an address", async () => {
+    assert.equal(
+      await FeeHandlerRouterInstance._whitelist.call(
+        whitelistAddress
+      ),
+      false
+    );
+
+    const whitelistTx = await FeeHandlerRouterInstance.adminSetWhitelist(
+      whitelistAddress,
+      true
+    );
+    assert.equal(
+      await FeeHandlerRouterInstance._whitelist.call(
+        whitelistAddress
+      ),
+      true
+    );
+    TruffleAssert.eventEmitted(whitelistTx, "WhitelistChanged", (event) => {
+      return (
+        event.whitelistAddress === whitelistAddress &&
+        event.isWhitelisted === true
+      );
+    });
+  });
+
+  it("should require admin role to set whitelist address", async () => {
+    await assertOnlyAdmin(
+      FeeHandlerRouterInstance.adminSetWhitelist,
+      whitelistAddress,
+      true
+    );
+  });
+
+  it("should return fee 0 if address whitelisted", async () => {
+    await FeeHandlerRouterInstance.adminSetWhitelist(
+      whitelistAddress,
+      true
+    );
+    await FeeHandlerRouterInstance.adminSetResourceHandler(
+      destinationDomainID,
+      resourceID,
+      BasicFeeHandlerInstance.address
+    );
+    await BasicFeeHandlerInstance.changeFee(Ethers.utils.parseEther("0.5"));
+
+    const depositData = Helpers.createERCDepositData(100, 20, recipientAddress);
+    let res = await FeeHandlerRouterInstance.calculateFee.call(
+      whitelistAddress,
+      originDomainID,
+      destinationDomainID,
+      resourceID,
+      depositData,
+      feeData
+    );
+    assert.equal(web3.utils.fromWei(res[0], "ether"), "0")
+    res = await FeeHandlerRouterInstance.calculateFee.call(
+      nonWhitelistAddress,
+      originDomainID,
+      destinationDomainID,
+      resourceID,
+      depositData,
+      feeData
+    );
+    assert.equal(web3.utils.fromWei(res[0], "ether"), "0.5")
+  });
+
+  it("should revert if whitelisted address provides fee", async () => {
+    await FeeHandlerRouterInstance.adminSetWhitelist(
+      whitelistAddress,
+      true
+    );
+    await FeeHandlerRouterInstance.adminSetResourceHandler(
+      destinationDomainID,
+      resourceID,
+      BasicFeeHandlerInstance.address
+    );
+    await BasicFeeHandlerInstance.changeFee(Ethers.utils.parseEther("0.5"));
+
+    const depositData = Helpers.createERCDepositData(100, 20, recipientAddress);
+    await Helpers.expectToRevertWithCustomError(
+      FeeHandlerRouterInstance.collectFee(
+        whitelistAddress,
+        originDomainID,
+        destinationDomainID,
+        resourceID,
+        depositData,
+        feeData, 
+        {
+          from: bridgeAddress,
+          value: Ethers.utils.parseEther("0.5").toString()
+        }
+      ),
+      "IncorrectFeeSupplied(uint256)"
+    );
+    await TruffleAssert.passes(
+      FeeHandlerRouterInstance.collectFee(
+        nonWhitelistAddress,
+        originDomainID,
+        destinationDomainID,
+        resourceID,
+        depositData,
+        feeData, 
+        {
+          from: bridgeAddress,
+          value: Ethers.utils.parseEther("0.5").toString()
+        }
+      ),
+    );
+  });
+
+  it("should not collect fee from whitelisted address", async () => {
+    await FeeHandlerRouterInstance.adminSetWhitelist(
+      whitelistAddress,
+      true
+    );
+    await FeeHandlerRouterInstance.adminSetResourceHandler(
+      destinationDomainID,
+      resourceID,
+      BasicFeeHandlerInstance.address
+    );
+    await BasicFeeHandlerInstance.changeFee(Ethers.utils.parseEther("0.5"));
+
+    const depositData = Helpers.createERCDepositData(100, 20, recipientAddress);
+    await TruffleAssert.passes(
+      FeeHandlerRouterInstance.collectFee(
+        whitelistAddress,
+        originDomainID,
+        destinationDomainID,
+        resourceID,
+        depositData,
+        feeData, 
+        {
+          from: bridgeAddress,
+          value: "0" 
+        }
+      ),
     );
   });
 });
