1401431b3a1de3990e53b1720ae48cab971d5e77
mpetrun5 <matija.petrunic@gmail.com>
2024-07-22 13:23:04 +0200

    Update docstrings

--- contracts/Retry.sol
+++ contracts/Retry.sol
@@ -5,15 +5,17 @@ pragma solidity 0.8.11;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract RetryContract is Ownable {
+contract Retry is Ownable {
 
     event Retry(uint8 sourceDomainID, uint8 destinationDomainID, uint256 blockHeight, bytes32 resourceID);
 
     /**
         @notice This method is used to trigger the process for retrying failed deposits on the MPC side.
         @notice Only callable by admin.
-        @param block Block height on origin chain which contains failed deposits.
-        @param domainID ID of the block domain. 
+        @param sourceDomainID ID of the retry source. 
+        @param destinationDomainID ID of the transfer destination. 
+        @param blockHeight Block height on origin chain which contains failed deposits.
+        @param resourceID Resource ID of transfers that are to be retried.
      */
     function retry(uint8 sourceDomainID, uint8 destinationDomainID, uint256 blockHeight, bytes32 resourceID) external onlyOwner {
         emit Retry(sourceDomainID, destinationDomainID, blockHeight, resourceID);
